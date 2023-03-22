789aabfa455e1101c8e43772d6514a698a8ecfb8
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-22 14:26:36 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -5,4 +5,22 @@
         int result = num1 + num2;
         return result;
     }
+
+    public static double DoubleSum(double num1, double num2)
+    {
+        double result = num1 + num2;
+        return result;
+    }
+
+    public static int DifferenceSum(int num1, int num2)
+    {
+        int result = num1 / num2;
+        return result;
+    }
+
+    public static double DifferenceDoubleSum(double num1, double num2)
+    {
+        double result = num1 / num2;
+        return result;
+    }
 }
