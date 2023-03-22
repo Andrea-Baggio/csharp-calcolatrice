d7e5abfe88059d2c2fea9b9905806e4eca40470e
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-22 14:58:35 +0100

    maggiore

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@
         return 0;
     }
 
-    public static double AbsoluteValueDouble(double num1, double num2)
+    public static double MinimumDouble(double num1, double num2)
     {
         if (num1 > num2)
         {
@@ -84,6 +84,32 @@
         else if (num1 < num2)
         {
             return num1;
+        }
+        return 0;
+    }
+
+    public static int Maximum(int num1, int num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;
+        }
+        else if (num1 < num2)
+        {
+            return num2;
+        }
+        return 0;
+    }
+
+    public static double MaximumDouble(double num1, double num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;
+        }
+        else if (num1 < num2)
+        {
+            return num2;
         }
         return 0;
     }
